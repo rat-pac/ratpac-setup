289dd086232b5186e219af6a0cba1394cac5a88c
Morgan Askins <maskins@ucdavis.edu>
2022-10-25 10:24:51 -0700

    GDML support

--- ratpacSetup.sh
+++ ratpacSetup.sh
@@ -242,7 +242,9 @@ function install(){
     git clone https://github.com/geant4/geant4.git --single-branch --branch geant4-11.0-release geant_src
     mkdir -p geant_build
     cd geant_build
-    cmake -DCMAKE_INSTALL_PREFIX=$prefix ../geant_src -DGEANT4_BUILD_EXPAT=OFF -DGEANT4_BUILD_MULTITHREADED=OFF -DGEANT4_USE_QT=ON -DGEANT4_INSTALL_DATA=ON -DGEANT4_INSTALL_DATA_TIMEOUT=15000 \
+    cmake -DCMAKE_INSTALL_PREFIX=$prefix ../geant_src -DGEANT4_BUILD_EXPAT=OFF \
+      -DGEANT4_BUILD_MULTITHREADED=OFF -DGEANT4_USE_QT=ON -DGEANT4_INSTALL_DATA=ON \
+      -DGEANT4_INSTALL_DATA_TIMEOUT=15000 -DGEANT4_USE_GDML=ON \
       && make -j$procuse \
       && make install
     cd ../
