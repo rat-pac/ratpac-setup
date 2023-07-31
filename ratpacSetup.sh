bb912fd2e6c63656cc3ac25cda88debb718ab345
Morgan Askins <maskins@ucdavis.edu>
2023-07-31 15:56:30 -0700

    Refactor setup script and update to latest g4

--- ratpacSetup.sh
+++ ratpacSetup.sh
@@ -10,217 +10,330 @@ exec > >(tee -i install.log)
 exec 2>&1
 
 function install(){
-  gitdir="git@github.com:oggroup/ratpac-two.git"
-  help $@
-  procuse=$(getnproc $@)
-  # End testing
-  export CC=$(command -v gcc)
-  export CXX=$(command -v g++)
+    ## Array of installables
+    declare -A install_options=("cmake" "root" "geant4" "cry" "tensorflow" "ratpac")
+    declare -A install_selection
+    for element in "${install_options[@]}"
+    do
+        install_selection[$element]=true
+    done
+    # Versioning
+    root_branch="v6-28-00-patches"
+    geant_branch="v11.2.0.beta"
+    ratpac_repository="git@github.com:oggroup/ratpac-two.git"
 
-  # Versioning
-  root_branch="v6-28-00-patches"
-  
-  # Check requirements; Git && GCC
-  if ! [ -x "$(command -v gcc)" ]; then
-    echo "gcc not installed"
-    exit 1
-  fi
-  if ! [ -x "$(command -v git)" ]; then
-    echo "git not installed"
-    exit 1
-  fi
+    help $@
+    procuse=$(getnproc $@)
+    # End testing
+    export CC=$(command -v gcc)
+    export CXX=$(command -v g++)
 
-  outfile="env.sh"
-  prefix=$(pwd -P)/local
-  mkdir -p $prefix/bin
-  export PATH=$prefix/bin:$PATH
-  export LD_LIBRARY_PATH=$prefix/lib:$LD_LIBRARY_PATH
-  printf "export PATH=$prefix/bin:\$PATH\n" > $outfile
-  printf "export LD_LIBRARY_PATH=$prefix/lib:\$LD_LIBRARY_PATH\n" >> $outfile
-  printf "export CC=$CC\n" >> $outfile
-  printf "export CXX=$CXX\n" >> $outfile
+    # Check requirements; Git && GCC
+    if ! [ -x "$(command -v gcc)" ]; then
+        echo "gcc not installed"
+        exit 1
+    fi
+    if ! [ -x "$(command -v git)" ]; then
+        echo "git not installed"
+        exit 1
+    fi
 
-  ## Tensorflow options
-  enable_gpu=false
-  enable_mac=false
-  
-  skipping=false
-  skip_cmake=false
-  skip_root=false
-  skip_geant=false
-  skip_ratpac=false
-  skip_cry=false
-  skip_tflow=false
-  for element in $@;
-  do
-    if [ "$skipping" = true ]
+    outfile="env.sh"
+    prefix=$(pwd -P)/local
+    mkdir -p $prefix/bin
+    export PATH=$prefix/bin:$PATH
+    export LD_LIBRARY_PATH=$prefix/lib:$LD_LIBRARY_PATH
+    printf "export PATH=$prefix/bin:\$PATH\n" > $outfile
+    printf "export LD_LIBRARY_PATH=$prefix/lib:\$LD_LIBRARY_PATH\n" >> $outfile
+    printf "export CC=$CC\n" >> $outfile
+    printf "export CXX=$CXX\n" >> $outfile
+
+    ## Tensorflow options
+    enable_gpu=false
+    enable_mac=false
+    cleanup=true
+    boolOnly=false
+    
+    for element in $@;
+    do
+        if [ "$skipping" = true ]
+        then
+            # Check if element in install_options
+            if [[ " ${install_options[@]} " =~ " ${element} " ]]
+            then
+                install_selection[$element]=false
+            fi
+        fi
+        if [ $element == "--skip" ]
+        then
+            skipping=true;
+        fi
+    done
+    
+    for element in $@;
+    do
+        if [ "$boolOnly" = true ]
+        then
+            if [[ " ${install_options[@]} " =~ " ${element} " ]]
+            then
+                install_selection[$element]=true
+            fi
+        fi
+        if [ $element == "--only" ]
+        then
+            # Only will overwrite the skipping rules
+            boolOnly=true
+            # Set all to false
+            for element in "${install_options[@]}"
+            do
+                install_selection[$element]=false
+            done
+        fi
+        if [ $element == "--noclean" ]
+        then
+            cleanup=false
+        fi
+        if [ $element == "--gpu" ]
+        then
+            enable_gpu=true
+        fi
+        if [ $element == "--mac" ]
+        then
+            enable_mac=true
+        fi
+    done
+    
+    # global options dictionary
+    declare -A global_options=(["procuse"]=$procuse ["prefix"]=$prefix ["root_branch"]=$root_branch \
+        ["geant_branch"]=$geant_branch ["enable_gpu"]=$enable_gpu ["enable_mac"]=$enable_mac \
+        ["ratpac_repository"]=$ratpac_repository ["cleanup"]=$cleanup)
+    if [ "${insall_options[cmake]}" = true ]
     then
-      if [ $element == "cmake" ]
-      then
-        skip_cmake=true
-      fi
-      if [ $element == "root" ]
-      then
-        skip_root=true
-      fi
-      if [ $element == "geant4" ]
-      then
-        skip_geant=true
-      fi
-      if [ $element == "ratpac" ]
-      then
-        skip_ratpac=true
-      fi
-      if [ $element == "cry" ]
-      then
-        skip_cry=true
-      fi
-      if [ $element == "tensorflow" ]
-      then
-        skip_tflow=true
-      fi
+        install_cmake global_options
     fi
-    if [ $element == "--skip" ]
+
+    if [ "${install_selection[root]}" = true ]
     then
-      skipping=true;
+        install_root global_options
     fi
-  done
-  
-  cleanup=true
-  boolOnly=false
-  for element in $@;
-  do
-    if [ "$boolOnly" = true ]
+
+    if [ "${install_selection[geant4]}" = true ]
     then
-      if [ $element == "cmake" ]
-      then
-        skip_cmake=false
-      fi
-      if [ $element == "root" ]
-      then
-        skip_root=false
-      fi
-      if [ $element == "geant4" ]
-      then
-        skip_geant=false
-      fi
-      if [ $element == "ratpac" ]
-      then
-        skip_ratpac=false
-      fi
-      if [ $element == "cry" ]
-      then
-        skip_cry=false
-      fi
-      if [ $element == "tensorflow" ]
-      then
-        skip_tflow=false
-      fi
+        install_geant4 global_options
     fi
-    if [ $element == "--only" ]
+
+    if [ "${install_selection[cry]}" = true ]
     then
-      # Only will overwrite the skipping rules
-      boolOnly=true
-      skip_cmake=true
-      skip_root=true
-      skip_geant=true
-      skip_ratpac=true
-      skip_cry=true
-      skip_tflow=true
+        install_cry global_options
     fi
-    if [ $element == "--noclean" ]
+
+    if [ "${install_selection[tensorflow]}" = true ]
     then
-      cleanup=false
+        install_tensorflow global_options
     fi
-    if [ $element == "--gpu" ]
+
+    if [ "${install_selection[ratpac]}" = true ]
     then
-      enable_gpu=true
+        install_ratpac global_options
     fi
-    if [ $element == "--mac" ]
+
+    if [[ -f "$prefix/lib/libCRY.so" ]];
     then
-      enable_mac=true
+        printf "export CRYLIB=$prefix/lib\n" >> $outfile
+        printf "export CRYINCLUDE=$prefix/include/cry\n" >> $outfile
+        printf "export CRYDATA=$prefix/data/cry\n" >> $outfile
     fi
-  done
-  
-  
-  # Install cmake
-  if ! [ "$skip_cmake" = true ]
-  then
+    printf "pushd $prefix/bin 2>&1 >/dev/null\nsource thisroot.sh\nsource geant4.sh\npopd 2>&1 >/dev/null\n" >> $outfile
+    printf "if [ -f \"$prefix/../ratpac/ratpac.sh\" ]; then\nsource $prefix/../ratpac/ratpac.sh\nfi" >> $outfile
+}
+
+function help()
+{
+    declare -A help_options=(["only"]="Only install the following packages" \
+        ["skip"]="Skip the following packages" \
+        ["gpu"]="Enable GPU support for tensorflow" \
+        ["mac"]="Enable Mac support for tensorflow" \
+        ["noclean"]="Do not clean up after install")
+    for element in $@
+    do
+        if [[ $element =~ "-h" ]];
+        then
+            printf "\nAvailable Packages\n"
+            for key in "${!install_options[@]}"
+            do
+                printf "%-20s%-20s\n" $key "${install_options[$key]}"
+            done
+            printf "\nOptions\n"
+            for key in "${!help_options[@]}"
+            do
+                printf "%-20s%-20s\n" --$key "${help_options[$key]}"
+            done
+            exit 0
+        fi
+    done
+}
+
+function getnproc()
+{
+    local nproc=1
+    for element in $@
+    do
+        if [[ $element =~ "-j" ]];
+        then
+            nproc=$(echo $element | sed -e 's/-j//g')
+        fi
+    done
+    echo $nproc
+}
+
+function command_exists()
+{
+    if (command -v $1 > /dev/null )
+    then
+        true
+    else
+        false
+    fi
+}
+
+function check_deps()
+{
+    bool=true
+    # Before trying to install anything, confirm a list of dependencies
+    echo "Checking list of dependencies ..."
+    cmds=(gcc openssl curl)
+    for c in ${cmds[@]}
+    do
+        if command_exists $c
+        then
+            printf "%-30s%-20s\n" $c "Installed"
+        else
+            printf "%-30s%-20s\n" $c "NOT AVAILABLE"
+            bool=false
+        fi
+    done
+    # Check libraries with ldd
+    echo "Checking for libraries ..."
+    libraries=(libX11 libXpm libXft libffi libXext libQt libOpenGL)
+    for lb in ${libraries[@]}
+    do
+        if check_lib $lb
+        then
+            printf "%-30s%-20s\n" $lb "Installed"
+        else
+            printf "%-30s%-20s\n" $lb "NOT AVAILABLE"
+            bool=false
+        fi
+    done
+    echo "Dependencies look to be in check"
+
+    $bool
+}
+
+function check_lib()
+{
+    if (ldconfig -p | grep -q $1)
+    then
+        true
+    else
+        false
+    fi
+}
+
+function skip_check()
+{
+    bool=false
+    for elem in $@
+    do
+        if [[ $elem = "--skip-checks" ]];
+        then
+            printf "Skipping dependency checker\n"
+            bool=true
+        fi
+    done
+    $bool
+}
+
+## Installation commands
+function install_cmake()
+{
+    declare -n options=$1
     git clone https://github.com/Kitware/CMake.git --single-branch --branch v3.22.0 cmake_src
     mkdir -p cmake_build
     cd cmake_build
     ../cmake_src/bootstrap --prefix=../local \
-      && make -j$procuse \
-      && make install
+        && make -j${options[procuse]} \
+        && make install
     cd ../
     # Check if cmake was successful, if so clean-up, otherwise exit
-    if test -f $prefix/bin/cmake
+    if test -f ${options[prefix]}/bin/cmake
     then
-      printf "Cmake install successful\n"
+        printf "Cmake install successful\n"
     else
-      printf "Cmake install failed ... check logs\n"
-      exit 1
+        printf "Cmake install failed ... check logs\n"
+        exit 1
     fi
-    if [ "$cleanup" = true ]
+    if [ "${options[cleanup]}" = true ]
     then
-      rm -rf cmake_src cmake_build
+        rm -rf cmake_src cmake_build
     fi
-  fi
-  
-  # Install root
-  if ! [ "$skip_root" = true ]
-  then
-    git clone https://github.com/root-project/root.git --single-branch --branch $root_branch root_src
+}
+
+function install_root()
+{
+    declare -n options=$1
+    git clone https://github.com/root-project/root.git --single-branch --branch ${options[root_branch]} root_src
     mkdir -p root_build
     cd root_build
-    cmake -DCMAKE_INSTALL_PREFIX=$prefix -D xrootd=OFF -D roofit=OFF -D minuit2=ON\
-        ../root_src \
-      && make -j$procuse \
-      && make install
+    cmake -DCMAKE_INSTALL_PREFIX=${options[prefix]} -D xrootd=OFF -D roofit=OFF -D minuit2=ON\
+            ../root_src \
+        && make -j${options[procuse]} \
+        && make install
     cd ../
     # Check if root was successful, if so clean-up, otherwise exit
-    if test -f $prefix/bin/root
+    if test -f ${options[prefix]}/bin/root
     then
-      printf "Root install successful\n"
+        printf "Root install successful\n"
     else
-      printf "Root install failed ... check logs\n"
-      exit 1
+        printf "Root install failed ... check logs\n"
+        exit 1
     fi
-    if [ "$cleanup" = true ]
+    if [ "${options[cleanup]}" = true ]
     then
-      rm -rf root_src root_build
+        rm -rf root_src root_build
     fi
-  fi
-  
-  # Install Geant4
-  if ! [ "$skip_geant" = true ]
-  then
-    git clone https://github.com/geant4/geant4.git --single-branch --branch geant4-11.0-release geant_src
+}
+
+function install_geant4()
+{
+    declare -n options=$1
+    git clone https://github.com/geant4/geant4.git --single-branch --branch ${options[geant_branch]} geant_src
     mkdir -p geant_build
     cd geant_build
-    cmake -DCMAKE_INSTALL_PREFIX=$prefix ../geant_src -DGEANT4_BUILD_EXPAT=OFF \
-      -DGEANT4_BUILD_MULTITHREADED=OFF -DGEANT4_USE_QT=ON -DGEANT4_INSTALL_DATA=ON \
-      -DGEANT4_INSTALL_DATA_TIMEOUT=15000 -DGEANT4_USE_GDML=ON \
-      && make -j$procuse \
-      && make install
+    cmake -DCMAKE_INSTALL_PREFIX=${options[prefix]} ../geant_src -DGEANT4_BUILD_EXPAT=OFF \
+        -DGEANT4_BUILD_MULTITHREADED=OFF -DGEANT4_USE_QT=ON -DGEANT4_INSTALL_DATA=ON \
+        -DGEANT4_INSTALL_DATA_TIMEOUT=15000 -DGEANT4_USE_GDML=ON \
+        && make -j${options[procuse]} \
+        && make install
     cd ../
     # Check if g4 was successful, if so clean-up, otherwise exit
-    if test -f $prefix/bin/geant4-config
+    if test -f ${options[prefix]}/bin/geant4-config
     then
-      printf "G4 install successful\n"
+        printf "G4 install successful\n"
     else
-      printf "G4 install failed ... check logs\n"
-      exit 1
+        printf "G4 install failed ... check logs\n"
+        exit 1
     fi
-    if [ "$cleanup" = true ]
+    if [ "${options[cleanup]}" = true ]
     then
-      rm -rf geant_src geant_build
+        rm -rf geant_src geant_build
     fi
-  fi
+}
 
-  # Install CRY for cosmogenics
-  if ! [ "$skip_cry" = true ]
-  then
+function install_cry()
+{
+    declare -n options=$1
+    # Install CRY for cosmogenics
     curl https://nuclear.llnl.gov/simulation/cry_v1.7.tar.gz --output cry.tar.gz
     tar xzvf cry.tar.gz
     cd cry_v1.7
@@ -229,176 +342,77 @@ function install(){
     sed -i '25 i \\t$(CXX) -shared $(OBJ) -o ../lib/libCRY.so' src/Makefile
     sed -i 's/\-Wall/\-Wall \-fPIC/g' src/Makefile
     make -j1 # Race condition using multiple threads
-    mkdir -p $prefix/data/cry
-    mv data/* $prefix/data/cry
+    mkdir -p ${options[prefix]}/data/cry
+    mv data/* ${options[prefix]}/data/cry
     # "Make install"
-    mv lib/libCRY.so $prefix/lib
-    mkdir -p $prefix/include/cry
-    cp src/*.h $prefix/include/cry
+    mv lib/libCRY.so ${options[prefix]}/lib
+    mkdir -p ${options[prefix]}/include/cry
+    cp src/*.h ${options[prefix]}/include/cry
     cd ../
-    if [ "$cleanup" = true ]
+    if [ "${options[cleanup]}" = true ]
     then
-      rm -r cry_v1.7 cry.tar.gz
+        rm -r cry_v1.7 cry.tar.gz
     fi
-  fi
+}
 
-  # Tensorflow
-  if ! [ "$skip_tflow" = true ]
-  then
+function install_tensorflow()
+{
+    declare -n options=$1
+    # Tensorflow
     # CPU only or GPU support, listen for the --gpu command? Also if macos?
     linuxGPU="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-2.9.1.tar.gz"
     linuxCPU="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-2.9.1.tar.gz"
     macCPU="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-2.9.1.tar.gz"
 
     tfurl=$linuxCPU #Default
-    if [ "$enable_gpu" = true ]
+    if [ "${options[enable_gpu]}" = true ]
     then
-      tfurl=$linuxGPU
+        tfurl=$linuxGPU
     fi
-    if [ "$enable_mac" = true ]
+    if [ "${options[enable_mac]}" = true ]
     then
-      tfurl=$macCPU
+        tfurl=$macCPU
     fi
     curl $tfurl --output tensorflow.tar.gz
-    tar -C $prefix -xzf tensorflow.tar.gz
+    tar -C ${options[prefix]} -xzf tensorflow.tar.gz
 
     git clone git@github.com:serizba/cppflow.git
-    cp -r cppflow/include/cppflow $prefix/include
+    cp -r cppflow/include/cppflow ${options[prefix]}/include
     rm -rf tensorflow.tar.gz cppflow
-  fi
-  
-  # Install rat-pac
-  if ! [ "$skip_ratpac" = true ]
-  then
-    source $prefix/bin/thisroot.sh
-    source $prefix/bin/geant4.sh
+}
+
+function install_ratpac()
+{
+    declare -n options=$1
+    # Install rat-pac
+    source ${options[prefix]}/bin/thisroot.sh
+    source ${options[prefix]}/bin/geant4.sh
     rm -rf ratpac
-    git clone $gitdir ratpac
+    git clone ${options[ratpac_repository]} ratpac
     cd ratpac
-    make -j$procuse && source ./ratpac.sh
+    make -j${options[procuse]} && source ./ratpac.sh
     # Check if ratpac was successful, if so clean-up, otherwise exit
     if test -f build/bin/rat
     then
-      printf "Ratpac install successful\n"
+        printf "Ratpac install successful\n"
     else
-      printf "Ratpac install failed ... check logs\n"
-      exit 1
+        printf "Ratpac install failed ... check logs\n"
+        exit 1
     fi
     cd ../
-  fi
-
-  if [[ -f "$prefix/lib/libCRY.so" ]];
-  then
-    printf "export CRYLIB=$prefix/lib\n" >> $outfile
-    printf "export CRYINCLUDE=$prefix/include/cry\n" >> $outfile
-    printf "export CRYDATA=$prefix/data/cry\n" >> $outfile
-  fi
-  printf "pushd $prefix/bin 2>&1 >/dev/null\nsource thisroot.sh\nsource geant4.sh\npopd 2>&1 >/dev/null\n" >> $outfile
-  printf "if [ -f \"$prefix/../ratpac/ratpac.sh\" ]; then\nsource $prefix/../ratpac/ratpac.sh\nfi" >> $outfile
 }
 
-function help()
-{
-  for element in $@
-  do
-    if [[ $element =~ "-h" ]];
-    then
-      printf "Ratpac Dependency Installer -- in progress\n"
-      exit 0
-    fi
-  done
-}
 
-function getnproc()
-{
-  local nproc=1
-  for element in $@
-  do
-    if [[ $element =~ "-j" ]];
-    then
-      nproc=$(echo $element | sed -e 's/-j//g')
-    fi
-  done
-  echo $nproc
-}
-
-function command_exists()
-{
-  if (command -v $1 > /dev/null )
-  then
-    true
-  else
-    false
-  fi
-}
-
-function check_deps()
-{
-  bool=true
-  # Before trying to install anything, confirm a list of dependencies
-  echo "Checking list of dependencies ..."
-  cmds=(gcc openssl curl)
-  for c in ${cmds[@]}
-  do
-    if command_exists $c
-    then
-      printf "%-30s%-20s\n" $c "Installed"
-    else
-      printf "%-30s%-20s\n" $c "NOT AVAILABLE"
-      bool=false
-    fi
-  done
-  # Check libraries with ldd
-  echo "Checking for libraries ..."
-  libraries=(libX11 libXpm libXft libffi libXext libQt libOpenGL)
-  for lb in ${libraries[@]}
-  do
-    if check_lib $lb
-    then
-      printf "%-30s%-20s\n" $lb "Installed"
-    else
-      printf "%-30s%-20s\n" $lb "NOT AVAILABLE"
-      bool=false
-    fi
-  done
-  echo "Dependencies look to be in check"
-
-  $bool
-}
-
-function check_lib()
-{
-  if (ldconfig -p | grep -q $1)
-  then
-    true
-  else
-    false
-  fi
-}
-
-function skip_check()
-{
-  bool=false
-  for elem in $@
-  do
-    if [[ $elem = "--skip-checks" ]];
-    then
-      printf "Skipping dependency checker\n"
-      bool=true
-    fi
-  done
-  $bool
-}
-
+## Main function with checks
 if skip_check $@
 then
-  install $@
-else
-  if check_deps
-  then
     install $@
-  else
-    printf "\033[31mPlease install system dependencies as indicated above.\033[0m\n"
-    printf "\033[31mYou can skip these checks by passing the --skip-checks flag.\033[0m\n"
-  fi
+else
+    if check_deps
+    then
+        install $@
+    else
+        printf "\033[31mPlease install system dependencies as indicated above.\033[0m\n"
+        printf "\033[31mYou can skip these checks by passing the --skip-checks flag.\033[0m\n"
+    fi
 fi
