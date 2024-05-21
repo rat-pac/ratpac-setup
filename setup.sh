ec5eaee78680215f65781cce8fbf85960203797c
Wilf Shorrock <w.shorrock@sussex.ac.uk>
2024-05-21 10:57:20 +0100

    Fix geant search for xerces
    Was using the wrong variable for finding the xerces library

--- setup.sh
+++ setup.sh
@@ -428,11 +428,11 @@ function install_geant4()
     then
         LIBSUFFIX="dylib"
     fi
-    cmake -DCMAKE_INSTALL_PREFIX=${options[prefix]} ../geant_src -DGEANT4_BUILD_EXPAT=OFF \
+    ../local/bin/cmake -DCMAKE_INSTALL_PREFIX=${options[prefix]} ../geant_src -DGEANT4_BUILD_EXPAT=OFF \
         -DGEANT4_BUILD_MULTITHREADED=OFF -DGEANT4_USE_QT=ON -DGEANT4_INSTALL_DATA=ON \
         -DGEANT4_BUILD_TLS_MODEL=global-dynamic \
         -DGEANT4_INSTALL_DATA_TIMEOUT=15000 -DGEANT4_USE_GDML=ON \
-        -DXercesC_INCLUDE_DIR=${options[prefix]}/include -DXercesC_LIBRARY=${options[prefix]}/lib/libxerces-c.${LIBSUFFIX} \
+        -DXercesC_INCLUDE_DIR=${options[prefix]}/include -DXercesC_LIBRARY_RELEASE=${options[prefix]}/lib/libxerces-c.${LIBSUFFIX} \
         && make -j${options[procuse]} \
         && make install
     cd ../
